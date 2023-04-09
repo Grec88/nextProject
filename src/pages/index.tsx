00130a1a4ed01c08a0dfb6a92369c5c688691bb9
Aleksey Likhtinov <91214571+Grec88@users.noreply.github.com>
2023-04-09 16:08:12 +0300

    header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { Htag } from '@/components';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <div>
-    adfgdfgsdg
+    <Htag tag="h2">sdgfsdg</Htag>
     </div>
   );
 }
