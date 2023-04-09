4066a6489b442e53662c29303bc21ee13ea5ee0e
Aleksey Likhtinov <91214571+Grec88@users.noreply.github.com>
2023-04-09 17:47:05 +0300

    Headers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import { Htag } from '@/components';
+
 
 
 const inter = Inter({ subsets: ['latin'] });
@@ -9,7 +9,9 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   return (
     <>
-    <Htag tag="h2">Header2</Htag>
+    <h1 className="header__1">Header2</h1>
+    <h2 className="header__2">Header2</h2>
+    <h3 className="header__3">Header2</h3>
     </>
   );
 }
