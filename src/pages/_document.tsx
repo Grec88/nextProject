1c91e48cf7c1832d5fc33515fb4a9536fb3a9e33
Aleksey Likhtinov <91214571+Grec88@users.noreply.github.com>
2023-04-09 13:43:14 +0300

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document(): JSX.Element {
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+        </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
