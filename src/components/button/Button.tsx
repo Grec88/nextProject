8c72a5b772d5d0182f20f428042599b34fa98ab2
unknown <Lihtinmann88@yandex.ru>
2023-05-06 20:44:08 +0300

    onClick props

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,14 +3,14 @@ import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 import ArrowIcon from './arrow.svg';
 
-export const Button = ({children, arrow='none', appearance, className, ...props}: ButtonProps): JSX.Element =>{
+export const Button = ({children, arrow='none', appearance, onClick, className}: ButtonProps): JSX.Element =>{
     return(
         <button className={cn(
             styles.button, 
             className,
             appearance === 'primary' && styles.primary,
             appearance == 'ghost' && styles.ghost
-        )} {...props}>
+        )} onClick={onClick}>
             {children}
             {arrow != 'none' && <span className={cn(styles.arrow, arrow === 'down' && styles.down)}>
                 <ArrowIcon />
